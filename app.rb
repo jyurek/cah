375b1ae6478e22c09d293d9fc3b70dbdd5cff700
Jon Yurek <jyurek@thoughtbot.com>
2012-12-13 11:13:50 -0500

    A running, empty app

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'sinatra'
 
+use Rack::Static, :urls => ['/css', '/js', '/images'], :root => 'public'
 set :views, settings.root + '/templates'
 
 get '/' do
